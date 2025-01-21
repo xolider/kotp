075a9279d40038bdfd499a053a1831bc1e84f342
Clément Vicart <clement.vicart@ext.adeo.com>
2025-01-21 13:55:22 +0100

    Added README.MD

--- src/main/kotlin/dev/vicart/kotp/hotp/HOTPVerifier.kt
+++ src/main/kotlin/dev/vicart/kotp/hotp/HOTPVerifier.kt
@@ -9,9 +9,7 @@ class HOTPVerifier(val algorithm: HashAlgorithm = HashAlgorithm.SHA1) {
 
     fun verify(key: SecretKey, code: String, counter: Long) : Boolean {
         val hotpGenerator = HOTPGenerator(code.length, algorithm)
-        return (counter-1..counter+1).map {
-            hotpGenerator.generateCode(key, it) == code
-        }.contains(true)
+        return hotpGenerator.generateCode(key, counter) == code
     }
 
     fun verify(key: ByteArray, code: String, counter: Long) : Boolean {
